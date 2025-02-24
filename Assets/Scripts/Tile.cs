8eb89a25b206de1e10491a5f601cb667a4d57422
tamjjae <hjwj33@naver.com>
2025-02-25 00:13:26 +0900

    (몬스터타일 발판 생성/빨간색)

--- Assets/Scripts/Tile.cs
+++ Assets/Scripts/Tile.cs
@@ -4,6 +4,22 @@ using UnityEngine;
 
 public class Tile : MonoBehaviour
 {
+    private GameObject obstacle;
+
+    public void SetObstacle(GameObject obstacle)
+    {
+        this.obstacle = obstacle;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player") && obstacle != null)
+        {
+            Destroy(obstacle);
+            obstacle = null;
+        }
+    }
+
     public bool TileOnLeft(Transform position)
     {
         return position.position.x > transform.position.x;
