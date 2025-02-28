232559ab7fea07130bf3b5afb2e41680bfa6757f
ShinDong-Soo <sh0819su@gmail.com>
2025-02-28 14:36:34 +0900

    몬스터 Die 애니메이션 코드 작업
    몬스터 Die 애니메이션 코드 작업하였습니다.

---
+++ Assets/Scripts/Monsters/MonsterAnimationController.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class MonsterAnimationController : MonoBehaviour
+{
+    private Animator animator;
+
+    private void Awake()
+    {
+        animator = GetComponentInChildren<Animator>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void PlayMonsterDieAnimation()
+    {
+        animator.SetTrigger("Death");
+    }
+
+
+    public void StartDeath(Monster monster)
+    {
+        StartCoroutine(DestrotyAfterAnimation(monster));
+    }
+    
+
+    private IEnumerator DestrotyAfterAnimation(Monster monster)
+    {
+        // Death 애니메이션 트리거 실행
+        PlayMonsterDieAnimation();
+
+        AnimatorStateInfo stateInfo = animator.GetCurrentAnimatorStateInfo(0);
+        float deathAnimationLength = stateInfo.length;
+
+        if (stateInfo.IsName("Death"))
+        {
+            deathAnimationLength = stateInfo.length;
+        }
+
+        yield return new WaitForSeconds(deathAnimationLength);
+
+    }
+}
